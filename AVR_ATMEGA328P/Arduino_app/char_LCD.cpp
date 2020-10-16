7c9c7f22101a4f9cbb016f89ef7c613509cb7df9
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-10-16 10:25:58 +0900

    [Add] Char_LCD

---
+++ AVR_ATMEGA328P/Arduino_app/char_LCD.cpp
@@ -0,0 +1,198 @@
+#include "char_LCD.h"
+
+//#define RS  3
+//#define RW  4 
+//#define E   5
+//#define DB0 6
+//#define DB1 7
+//#define DB2 8
+//#define DB3 9
+//#define DB4 10
+//#define DB5 11
+//#define DB6 12
+//#define DB7 13
+
+char RS;
+char RW;
+char E;
+char DB[8];
+
+//8bit
+//----------------------------------------------------------------
+void char_lcd_init_8bit(char *pin)
+{
+	for (int i = 0; i < 11; i++) pinMode(pin[i], OUTPUT);
+	RS = pin[0];
+	RW = pin[1];
+	E = pin[2];
+	for (int i = 0; i < 8; i++) DB[i] = pin[i + 3];
+
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x38);
+	char_lcd_inst_8bit(0x06);
+	char_lcd_inst_8bit(0x0C);
+	char_lcd_inst_8bit(0x80);
+	char_lcd_inst_8bit(0x01);
+	delay(10);
+}
+
+void char_lcd_inst_8bit(char inst)
+{
+	digitalWrite(RS, LOW);
+	
+	for (int i = 0; i < 8; i++)
+	{
+		if (inst & (0x01 << 1))
+		{
+			digitalWrite(DB[i], HIGH);
+		}
+		else
+		{
+			digitalWrite(DB[i], LOW);
+		}
+	}
+
+	digitalWrite(E, HIGH);
+	digitalWrite(E, LOW);
+
+	digitalWrite(RS, HIGH);
+
+	delayMicroseconds(50);
+}
+
+
+void char_lcd_data_8bit(char data)
+{
+
+	for (int i = 0; i < 8; i++)
+	{
+		if (data & (0x01 << 1))
+		{
+			digitalWrite(6 + i, HIGH);
+		}
+		else
+		{
+			digitalWrite(6 + i, LOW);
+		}
+	}
+
+	digitalWrite(E, HIGH);
+	digitalWrite(E, LOW);
+
+	digitalWrite(RS, HIGH);
+}
+
+//----------------------------------------------------------------
+
+//4bit
+//----------------------------------------------------------------
+void char_lcd_init_4bit(char* pin)
+{
+	for (int i = 0; i < 7; i++) pinMode(pin[i], OUTPUT);
+	RS = pin[0];
+	RW = pin[1];
+	E = pin[2];
+	for (int i = 0; i < 8; i++) DB[i] = pin[3 + i];
+
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x30);
+	char_lcd_inst_8bit(0x28);
+
+	char_lcd_inst_4bit(0x28);
+	char_lcd_inst_4bit(0x06);
+	char_lcd_inst_4bit(0x0C);
+	char_lcd_inst_4bit(0x80);
+	char_lcd_inst_4bit(0x01);
+	
+	delay(10);
+}
+
+void char_lcd_inst_4bit(char inst)
+{
+	char_lcd_inst_4bit_HIGH(inst);
+	char_lcd_inst_4bit_LOW(inst);
+}
+
+void char_lcd_inst_4bit_HIGH(char inst_high)
+{
+	digitalWrite(RS, LOW);
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (inst_high & (0x10 << 1))
+		{
+			digitalWrite(DB[i], HIGH);
+		}
+		else
+		{
+			digitalWrite(DB[i], LOW);
+		}
+	}
+
+	digitalWrite(E, HIGH);
+	digitalWrite(E, LOW);
+
+	digitalWrite(RS, HIGH);
+
+	delayMicroseconds(50);
+}
+
+void char_lcd_inst_4bit_LOW(char inst_low)
+{
+	digitalWrite(RS, LOW);
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (inst_low & (0x01 << 1))
+		{
+			digitalWrite(DB[i], HIGH);
+		}
+		else
+		{
+			digitalWrite(DB[i], LOW);
+		}
+	}
+
+	digitalWrite(E, HIGH);
+	digitalWrite(E, LOW);
+
+	digitalWrite(RS, HIGH);
+
+	delayMicroseconds(50);
+}
+
+
+void char_lcd_data_4bit_HIGH(char data_high)
+{
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (data_high & (0x10 << 1))
+		{
+			digitalWrite(DB[i], HIGH);
+		}
+		else
+		{
+			digitalWrite(DB[i], LOW);
+		}
+	}
+
+	digitalWrite(E, HIGH);
+	digitalWrite(E, LOW);
+
+	digitalWrite(RS, HIGH);
+}
+
+void char_lcd_data_4bit_LOW(char data_low)
+{
+
+}
+
+void char_lcd_data_4bit(char data)
+{
+  char_lcd_data_4bit_HIGH(data);
+  char_lcd_data_4bit_LOW(data);
+}
---
+++ AVR_ATMEGA328P/Arduino_app/char_LCD.h
@@ -0,0 +1,17 @@
+#ifndef CHAR_LCD_H
+#define CHAR_LCD_H
+
+#include "Arduino.h"
+
+// 8bit
+void char_lcd_init_8bit(char *pin);
+void char_lcd_inst_8bit(char inst);
+void char_lcd_data_8bit(char data);
+
+// 4bit
+void char_lcd_init_4bit(char* pin); 
+void char_lcd_inst_4bit(char inst);
+void char_lcd_inst_4bit_HIGH(char inst_high);
+void char_lcd_inst_4bit_LOW(char inst_low);
+
+#endif
