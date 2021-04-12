9206af300791b008e1c7d52e19c8385ed25766b4
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-12 18:14:42 +0900

    [MAKE] OLED Library
    2학년 수업 시간에 배운 OLED를 라이브러리로 만들었습니다.

---
+++ AVR/EXAM/OLED.h
@@ -0,0 +1,46 @@
+#ifndef OLED_H
+#define OLED_H
+
+#include "Arduino.h"
+
+#include "font.h"
+#include "exam_top.h"
+#include "exam_bottom.h"
+
+#define OLED_CS   9
+#define OLED_RST  8
+#define OLED_DC   10
+
+#define OLED_SCK  52
+#define OLED_DATA 51
+
+#define RED    0xf800
+#define GREEN  0x07e0
+#define BLUE   0x001f
+#define WHITE  0xffff
+#define BLACK  0x0000
+
+void oled_set();
+void spi_init();
+void spi_write(char data);
+void Write_Command(char command);
+void Write_Data(char data);
+void clear_screen(unsigned short color);
+void draw_bitmap_top();
+void draw_bitmap_bottom();
+void font_write(char x, char y, unsigned short color, char font);
+void string_write(char x, char y, unsigned short color, char *str);
+void draw_line_hori(int x_start, int x_end, int y, unsigned short color);
+void draw_line_vert(int y_start, int y_end, int x, unsigned short color);
+void delete_rect(char x, char y);
+void make_rect(char x, char y);
+void clear_string();
+void draw_mouse_pointer(char x, char y);
+void mouse_pointer_move(char x, char y, char c_choice);
+void draw_save_background(char x, char y);
+int selection_area(char x, char y);
+void draw_selection_click(int select);
+void delete_selection_click();
+void main_menu();
+
+#endif
