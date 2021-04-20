fb561398c58e11ca1d8316a90541979f9a279828
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-20 16:06:39 +0900

    [ADD] OpenCV 공유 라이브러리
    < 20210413 >
    OpenCV로 사진 촬영, 동영상 촬영, 얼굴 인식, 눈동자 인식, 사진 흑백 변환 함수를 각각 구현하여 공유라이브러리로 만들었습니다.

---
+++ Linux/Librarys/opencv_shared_library/opencv.h
@@ -0,0 +1,15 @@
+#ifndef OPENCV_H
+#define OPENCV_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void take_picture(string file_name);
+void take_video(string file_name);
+void face_detection();
+void eyes_detection();
+void iamge_save();
+
+#endif
