774755d78f5e4abe28abb5697294f6f4eb04ecf0
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2021-04-20 16:04:10 +0900

    [MAKE] 라이브러리 제작 실습
    < 20210406 >
    임베디드 리눅스 수업에서 배운
    라이브러리 실습에 사용된 코드와 파일입니다.

---
+++ Linux/Librarys/test.s
@@ -0,0 +1,44 @@
+	.file	"test.c"
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$1, -4(%rbp)
+	jmp	.L2
+.L4:
+	movl	-4(%rbp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
+	jne	.L3
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+.L3:
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$10, -4(%rbp)
+	jle	.L4
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
