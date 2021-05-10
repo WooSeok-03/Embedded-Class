aac61cea2075398d62446f047bf77037087dc21e
WooSeok-03 <7458dntjr@gmail.com>
2021-05-10 15:00:49 +0900

    [FEAT] Flask - 템플릿
    2021-05-10
    render_template함수를 사용하여, HTML 파일을 띄워주도록 하였다.

---
+++ Linux/Flask/app_render_template.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8 -*-
+# 모듈 로딩 ------------------------
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import time
+
+# Flask 객체 생성
+# __name__ => python file name
+app = Flask(__name__)
+
+# LED Settings
+LED = 17
+
+GPIO.setmode(GPIO.BCM)      # Pin 명명법 - BCM
+GPIO.setup(LED, GPIO.OUT)    # Pin 동작모드 설정 - Output ( LED )
+
+# http://localhost:5000/ -----------------------------------------------
+@app.route("/")
+def helloworld():
+    #return "<HTML><BODY bgcolor='yellow'><H1>HELLO WORLD!</H1></BODY></HTML>"
+    return render_template("helloworld.html") # HTML 파일을 rendering해주는 함수
+    # /templates 디렉토리에 있는 HTML 파일이름을 적어준다. 
+
+# http://localhost:5000/<user> -----------------------------------------
+@app.route("/<user>")
+def hello_user(user):
+    #return "<HTML><BODY bgcolor='pink'><H1>HELLO user</H1></BODY></HTML>"
+    return render_template("user.html", name=user) # HTML에 있는 {{name}}에 user가 들어감.
+    
+    
+
+# python 파일이 실행되는 경우 if문 아래 코드 실행
+if __name__ == '__main__':
+    # Flask Server Run
+    app.run(host="0.0.0.0")
+    GPIO.cleanup()  # Pin 사용 헤제 
+    exit(0)
