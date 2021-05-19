5d496e9ff172b5a37a94d9778e24be62b2921cc4
WooSeok-03 <7458dntjr@gmail.com>
2021-05-19 19:56:18 +0900

    [FEAT] Shell Script - 출력
    2021-05-19
    쉘스크립트의 기본문법
    echo로 출력하는 방법을 배웠습니다.

---
+++ Linux/Shell_Script/EXSHELL/ex_01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Output Variable Value ---------------------------------
+echo "\$HOME =>" $HOME
+
+echo "$USER =>" $USER
+echo '$USER =>' "$USER"
+echo '${USER} =>' "${USER}"
+echo '${USER} =>' ${USER}
+
+exit 0
+
